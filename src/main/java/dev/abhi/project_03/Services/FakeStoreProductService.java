eb6a2016319bade590461160928d6bea81908fc3
abhilash <abhilashreddyparishapally@gmail.com>
2025-02-25 02:55:16 +0530

    added fakestoreDtos

--- src/main/java/dev/abhi/project_03/Services/FakeStoreProductService.java
+++ src/main/java/dev/abhi/project_03/Services/FakeStoreProductService.java
@@ -6,6 +6,7 @@ import dev.abhi.project_03.dtos.FakeStoreProductDto;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -28,7 +29,14 @@ public class FakeStoreProductService implements ProductService{
 
     @Override
     public List<Product> getAllProducts() {
-        return List.of();
+       FakeStoreProductDto[] fakeStoreProductDtos=
+               restTemplate.getForObject("https://fakestoreapi.com/products",
+                       FakeStoreProductDto[].class);
+       List<Product> products=new ArrayList<>();
+       for(FakeStoreProductDto fakeStoreProductDto: fakeStoreProductDtos){
+           products.add(convertFakeStoreProductDtoToProduct(fakeStoreProductDto));
+       }
+        return products;
     }
 
     private Product convertFakeStoreProductDtoToProduct(FakeStoreProductDto fakeStoreProductDto) {
